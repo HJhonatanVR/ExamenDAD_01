ae6adf349b96ed49c62853d3b315a6bd83797928
HJhonatanVR <HJhonatanVR@Asus-HJhonatanVR.localdomain>
2022-04-27 11:27:17 -0500

    Primer examen DAD

---
+++ src/pe/edu/upeu/ejerc2/Cuenta.java
@@ -0,0 +1,26 @@
+package pe.edu.upeu.ejerc2;
+
+/**
+ *
+ * @author HJhonatanVR
+ */
+public class Cuenta {
+
+    private int saldo;
+    private int saldoInicial;
+
+    public Cuenta(int saldo){
+        this.saldoInicial=saldo;
+        this.saldo=saldo;
+    }
+    public synchronized void hacerMovimiento(int cantidad){
+        this.saldo = this.saldo+cantidad;
+    }
+    public boolean esSimulacionCorrecta(){
+        if (this.saldo==this.saldoInicial) return true;
+        return false;
+    }
+    public int getSaldo(){
+        return this.saldo;
+    }
+}
---
+++ src/pe/edu/upeu/ejerc3/Productor.java
@@ -0,0 +1,19 @@
+package pe.edu.upeu.ejerc3;
+
+/**
+ *
+ * @author HJhonatanVR
+ */
+public class Productor implements Runnable{
+        Cola colaCompartida;
+        public Productor(Cola cola){
+                this.colaCompartida=cola;
+        }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+       
+        
+} /*Fin de la clase*/
